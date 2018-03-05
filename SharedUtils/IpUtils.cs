54dc77f7000acaee8faa83107d051fbda9696c7e
Евгений <Dozer95@bk.ru>
2018-03-05 22:30:39 +0500

    Add SharedUtils

---
+++ SharedUtils/IpUtils.cs
@@ -0,0 +1,61 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace SharedUtils
+{
+    public static class IpUtils
+    {
+        public static IPAddress GetLocalIp(string hostName)
+        {
+            IPHostEntry host;
+            try
+            {
+                host = Dns.GetHostEntry(hostName);
+            }
+            catch (SocketException)
+            {
+                return null;
+            }
+
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip;
+                }
+            }
+
+            return null;
+        }
+
+        public static bool ValidateParams(string serverIp, string serverName, out string errorMessage)
+        {
+            if (serverIp == "" && serverName == "")
+            {
+                errorMessage = "Необходимо указать имя или ip-адрес сервера";
+                return false;
+            }
+
+            if (serverIp != "")
+            {
+                if (!IPAddress.TryParse(serverIp, out var _))
+                {
+                    errorMessage = "Указан некорректный ip-адрес сервера";
+                    return false;
+                }
+            }
+
+            if (serverName != "")
+            {
+                if (GetLocalIp(serverName) == null)
+                {
+                    errorMessage = $"Не удалось получить ip-адрес компьютера с именем {serverName}";
+                    return false;
+                }
+            }
+
+            errorMessage = "";
+            return true;
+        }
+    }
+}
--- UdpClient/ClientForm.cs
+++ UdpClient/ClientForm.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Windows.Forms;
 using System.Net.Sockets;
 using System.Text;
+using SharedUtils;
 
 namespace UdpClient
 {
@@ -19,27 +20,13 @@ namespace UdpClient
         private void ClientForm_Load(object sender, EventArgs e)
         {
             tbPcName.Text = Dns.GetHostName();
-            tbIpAddress.Text = GetLocalIp(tbPcName.Text);
+            tbIpAddress.Text = IpUtils.GetLocalIp(tbPcName.Text).ToString();
 
             lbStatus.Text = "Клиент запущен";
         }
 
-        private string GetLocalIp(string hostName)
+        private async void SendMessage(IPAddress serverIp, string message)
         {
-            var host = Dns.GetHostEntry(hostName);
-            foreach (var ip in host.AddressList)
-            {
-                if (ip.AddressFamily == AddressFamily.InterNetwork)
-                {
-                    return ip.ToString();
-                }
-            }
-            return "Не удалось получить IP";
-        }
-
-        private async void SendMessage(string ipAddress, string message)
-        {
-            var serverIp = IPAddress.Parse(ipAddress);
             var endPoint = new IPEndPoint(serverIp, ServerPort);
             var updClient = new System.Net.Sockets.UdpClient(ClientPort);
 
@@ -52,14 +39,14 @@ namespace UdpClient
             {
                 MessageBox.Show(this,
                     "Не удалось подключиться к удаленному серверу. Проверьте правильность ip-адреса или имени сервера.",
-                    "Сообщение не доставлено", 
+                    "Сообщение не доставлено",
                     MessageBoxButtons.OK,
                     MessageBoxIcon.Information);
             }
             catch (Exception ex)
             {
                 MessageBox.Show(this,
-                    "Во время отправки сообщения возникла непредвиденная ошибка:\n"+ex.Message,
+                    "Во время отправки сообщения возникла непредвиденная ошибка:\n" + ex.Message,
                     "Сообщение не доставлено",
                     MessageBoxButtons.OK,
                     MessageBoxIcon.Error);
@@ -70,42 +57,21 @@ namespace UdpClient
             }
         }
 
-        private bool ValidateParams(out string errorMessage)
-        {
-            if (tbServerIp.Text == "" && tbServerName.Text == "")
-            {
-                errorMessage = "Необходимо указать имя или ip-адрес сервера";
-                return false;
-            }
-
-            if (tbServerIp.Text != "")
-            {
-                if (!IPAddress.TryParse(tbServerIp.Text, out var _))
-                {
-                    errorMessage = "Указан некорректный ip-адрес сервера";
-                    return false;
-                }
-            }
-
-            errorMessage = "";
-            return true;
-        }
-
         private void btnSend_Click(object sender, EventArgs e)
         {
-            if (!ValidateParams(out var errorMessage))
+            if (!IpUtils.ValidateParams(tbServerIp.Text, tbServerName.Text, out var errorMessage))
             {
                 MessageBox.Show(this,
                     errorMessage,
-                    "Ошибка",
+                    "Некорректые параметры",
                     MessageBoxButtons.OK,
                     MessageBoxIcon.Information);
                 return;
             }
 
             var serverIp = tbServerIp.Text != ""
-                ? tbServerIp.Text
-                : GetLocalIp(tbPcName.Text);
+                ? IPAddress.Parse(tbServerIp.Text)
+                : IpUtils.GetLocalIp(tbServerName.Text);
 
             SendMessage(serverIp, tbMessage.Text);
         }
